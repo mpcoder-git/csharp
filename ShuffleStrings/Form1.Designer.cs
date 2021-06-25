5b43e36589985db49afcc12d2a63191ee8deff1a
mpcoder-git <mpcoder@mail.ru>
2021-06-25 12:21:18 +0600

    Утилита перемешивалка строк
    первый коммит

---
+++ ShuffleStrings/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace ShuffleStrings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(25, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 342);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(326, 29);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(245, 342);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Входной набор";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Выходной набор";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Загрузить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            this.openFileDialog1.Title = "Выберите текстовый файл";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.FileName = "out";
+            this.saveFileDialog1.Filter = "text files(*.txt)|*.txt|All files(*.*)|*.*";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 446);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(593, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Перемешать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(440, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Выгрузить данные";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Количество строк";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(323, 383);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Количество строк";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 468);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Перемешиватель строк";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ShuffleStrings/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace ShuffleStrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            statusStrip1.Items[0].Text = "Готов к работе";
+            statusStrip1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            openFileDialog1.ShowDialog();
+
+            string filename = openFileDialog1.FileName;
+
+            statusStrip1.Items[0].Text = "Работаю...";
+            statusStrip1.Refresh();
+
+
+            //MessageBox.Show(filename);
+            listBox1.Items.Clear();
+ 
+            string[] nabor1 = new string[1];
+            
+
+            string path = @filename;
+
+          
+            // асинхронное чтение
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                int count = 0;
+                while ((line =  sr.ReadLine()) != null)
+                {
+                    nabor1[count] = line;
+                    Array.Resize(ref nabor1, nabor1.Length + 1);
+                    //Console.WriteLine(line);
+                    count = count + 1;
+                }
+                Array.Resize(ref nabor1, nabor1.Length - 1);
+                //MessageBox.Show(Convert.ToString(nabor1.Length));
+                //listBox1.Items.AddRange(nabor1);
+                listBox1.Items.AddRange(nabor1);
+
+                label3.Text = "Количество строк: " + Convert.ToString(listBox1.Items.Count);
+                label3.Refresh();
+
+                
+                string[] nabor2 = new string[nabor1.Length];
+                
+
+                Array.Copy(nabor1, nabor2, nabor1.Length);
+
+                Random randomize = new Random();
+                string nline;
+                for (int i = 0; i < nabor2.Length-1; i++)
+                {
+
+                    
+                    int rnd = randomize.Next(nabor2.Length - 1);
+
+                    if (i!=rnd)
+                    {
+                        nline = nabor2[i];
+                        nabor2[i] = nabor2[rnd];
+                        nabor2[rnd] = nline;
+
+
+                    }
+
+                }
+
+                listBox2.Items.Clear();
+                listBox2.Items.AddRange(nabor2);
+
+                label4.Text = "Количество строк: " + Convert.ToString(listBox2.Items.Count);
+                label4.Refresh();
+            }
+
+            statusStrip1.Items[0].Text = "Готов к работе";
+            statusStrip1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            statusStrip1.Items[0].Text = "Работаю..";
+            statusStrip1.Refresh();
+            string[] nabor1 = new string[1];
+            string[] nabor2 = new string[1];
+            Array.Resize(ref nabor1, listBox1.Items.Count);
+
+            for (int i = 0; i < listBox1.Items.Count - 1; i++)
+            {
+
+                nabor1[i] = Convert.ToString(listBox1.Items[i]);
+
+            }
+
+            Array.Resize(ref nabor2, nabor1.Length);
+            Array.Copy(nabor1, nabor2, nabor1.Length);
+            Array.Resize(ref nabor2, nabor1.Length-1);
+
+
+            Random randomize = new Random();
+            string nline;
+            for (int i = 0; i < nabor2.Length - 1; i++)
+            {
+
+                
+                int rnd = randomize.Next(nabor2.Length - 1);
+
+                if (i != rnd)
+                {
+                    nline = nabor2[i];
+                    nabor2[i] = nabor2[rnd];
+                    nabor2[rnd] = nline;
+
+
+                }
+
+            }
+
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(nabor2);
+            statusStrip1.Items[0].Text = "Готов к работе";
+            statusStrip1.Refresh();
+        }
+
+ 
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < listBox1.Items.Count - 1; i++)
+            {
+                sb.Append(listBox2.Items[i]+"\n");
+            }
+
+            string s = sb.ToString();
+            System.IO.File.WriteAllText(filename, s);
+            MessageBox.Show("Файл сохранен");
+        }
+    }
+}
